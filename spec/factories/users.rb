1e6e492010e27c3449e973748fd2fca2534da83f
Matheus Ferreira <matheusmlfg@gmail.com>
2025-01-04 16:41:22 -0300

    Removes unnecessary association of profile in User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,5 @@ FactoryBot.define do
     name { Faker::Name.name }
     cpf { Faker::CPF.pretty }
     birthdate { Faker::Date.birthday(min_age: 18, max_age: 65) }
-
-    association :profile
   end
 end
