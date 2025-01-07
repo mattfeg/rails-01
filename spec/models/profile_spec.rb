517defb50485e177d3626ed00f0dfe9b2eed4c5a
Matheus Ferreira <matheusmlfg@gmail.com>
2025-01-07 10:21:02 -0300

    Refacts Models specs to use 'is_expected.to' instead of 'should'

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:user) }
-    it { should allow_value(true).for(:is_active) }
+    it { is_expected.to validate_presence_of(:user) }
+    it { is_expected.to allow_value(true).for(:is_active) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "validations" do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:cpf) }
-    it { should validate_presence_of(:birthdate) }
-    it { should allow_value("2024-12-12").for(:birthdate) }
-    it { should have_one(:profile).dependent(:destroy) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:cpf) }
+    it { is_expected.to validate_presence_of(:birthdate) }
+    it { is_expected.to allow_value("2024-12-12").for(:birthdate) }
+    it { is_expected.to have_one(:profile).dependent(:destroy) }
   end
 end
